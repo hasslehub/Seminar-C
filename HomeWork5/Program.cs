6f75d378174a414b730e1e353d2f555f79fc56e5
Ivan <hassled@mail.ru>
2022-06-22 20:54:47 +0300

    Home Work 5

--- HomeWork5/Program.cs
+++ HomeWork5/Program.cs
@@ -170,24 +170,27 @@ void FindFromArray2(int[] currentArray)
 {
     bool flag = false;
     int j = 0;
-
-    for (int i = 0; i < currentArray.Length; i++)
+    if (d > 100 && d < 1000)
     {
-        if (currentArray[i] == (d / 100) % 10) //
+        for (int i = 0; i < currentArray.Length; i++)
         {
-            j = i + 1;
-            if (currentArray[j] == (d / 10) % 10)
+            if (currentArray[i] == (d / 100) % 10) //
             {
-                j++;
-                if (currentArray[j] == d % 10)
+                j = i + 1;
+                if (currentArray[j] == (d / 10) % 10)
                 {
-                    flag = true;
+                    j++;
+                    if (currentArray[j] == d % 10)
+                    {
+                        flag = true;
+                    }
                 }
             }
         }
+        if (flag) Console.WriteLine($"В массиве есть последовательность {d}");
+        else Console.WriteLine($"В массиве НЕТ последовательности {d}");
     }
-    if (flag) Console.WriteLine($"В массиве есть последовательность {d}");
-    else Console.WriteLine($"В массиве НЕТ последовательности {d}");
+    else Console.WriteLine($"Число {d} не трёхзначное ");
 }
 NewRandomArray3(zadacha1);
 PrintArray(zadacha1);
@@ -197,18 +200,18 @@ FindFromArray2(zadacha1);
 На вход подаются два числа случайной длины. 
 Найдите произведение каждого разряда первого числа 
 на каждый разряд второго. Ответ запишите в массив.
-
 24, 132 -> {2, 6, 4, 4, 12, 8}
 */
 
 Console.WriteLine();
 Console.WriteLine("Задача 2");
 Random rand = new Random();
-int e = rand.Next(1, 1000);
-int f = rand.Next(1, 1000);
-int g = Len(e) + Len(f);
+int e = rand.Next(1, 10000); // 1 число случайной длины
+int f = rand.Next(1, 10000); // 2 число случайной длины
+int g = Len(e) * Len(f);    // общая длина массива с результатом
 
-int Len(int a)
+
+int Len(int a)  // определяем длину массива для каждого числа
 {
     int x = 0;
     while (a > 0)
@@ -219,32 +222,91 @@ int Len(int a)
     return x;
 }
 int[] zadacha2 = new int[g];
-int[] zadacha21 = new int[Len(e)];
-int[] zadacha22 = new int[Len(f)];
-//viod Fil(int[] ar1, int a)
-//{
-    //int len = ar1.Length;
-    while (e > 0)
+int[] temp1 = new int[Len(e)];  // массив для первого числа
+int[] temp2 = new int[Len(f)];  // массив для второго числа
+void Fil(int[] ar1, int a)      // переводим числа в массив
+{
+    int len = ar1.Length;
+    int i = 0;
+    while (a > 0)
     {
-        e = a % 10;
-        for (int i = 0; i < zadacha21.Length; i++)
+        int res = a;
+        res = res % 10;
+        if (i < len)
         {
-            zadacha21[zadacha21.Length - i-1] = a;
+            ar1[len - i - 1] = res;
         }
-        e = e / 10;
+        a = a / 10;
+        i++;
     }
-//}
-void Res(int[] currentarray, int m, int n)
+}
+void Res(int[] currentarray1, int[] currentarray2, int b) // перемножаем элементы массивов
 {
 
+    for (int i = 0; i < currentarray1.Length; i++)
+    {
+        for (int j = 0; j < currentarray2.Length; j++)
+        {
+            zadacha2[zadacha2.Length - b] = currentarray1[i] * currentarray2[j];
+            b--;
+
+        }
+    }
 }
 Console.WriteLine($"Заданы два числа: {e}, {f}");
 Console.WriteLine($"g = {g}");
-//Fil(zadacha21, e);
-PrintArray(zadacha21);
+Fil(temp1, e);
+Fil(temp2, f);
+PrintArray(temp1);
+PrintArray(temp2);
+Res(temp1, temp2, g);
+PrintArray(zadacha2);
+
+
 
 
 /* Задача 3. 
 Найдите все числа от 1 до 1000000, сумма цифр которых 
 в три раза меньше их произведений. Подсчитайте их количество.
 */
+
+
+Console.WriteLine();
+Console.WriteLine("Задача 2");
+
+
+double res = 0;
+double resSum = 0;
+double resMulti = 1;
+int j = 1;
+
+for (int i = 0; i < 1000000; i++)
+{
+    int v = i + 1;
+    int w = v;
+
+    while (w > 0)
+    {
+        resSum = resSum + w % 10;
+        resMulti = resMulti * (w % 10);
+        w = w / 10;
+    }
+    if (resSum > 0) res = resMulti / resSum;
+
+    if (res == 3)
+    {
+        Console.Write($"Кол-во = {j}  ");
+        Console.Write($"число = {v} ");
+        Console.Write($"Сумма цифр = {resSum}  ");
+        Console.Write($"Произ-ние = {resMulti}  ");
+        Console.WriteLine($"{resMulti}/{resSum} = {res}  ");
+        j++;
+    }
+
+    resSum = 0;
+    resMulti = 1;
+
+}
+Console.WriteLine();
+Console.WriteLine($"Кол-во чисел, сумма цифр которых в три раза меньше их произведений = {j - 1}");
+Console.WriteLine();
